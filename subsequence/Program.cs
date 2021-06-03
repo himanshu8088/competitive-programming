c821648a24caff0907328670f438d735fc4ea8dd
Himanshu Soni <himanshusoni1234@gmail.com>
2021-06-03 20:23:53 +0530

    add subsequence program

---
+++ subsequence/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace subsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            sol.Subsequence("123", "");      
+        }
+    } 
+
+    public class Solution{
+
+        public void Subsequence(string input, string output){
+            if(input.Length==0){
+                Console.WriteLine(output);
+                return;
+            }
+
+            Subsequence(input.Substring(1), string.Concat(output,input[0]));
+            Subsequence(input.Substring(1), output);
+        }
+    }
+}
